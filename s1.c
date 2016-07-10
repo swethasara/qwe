f919aa3ed3a196404099434ab68c8e96b3980c4b
swethasara <swethasaravanan0@gmail.com>
2016-07-10 12:49:38 +0530

    Create s1.c

---
+++ s1.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void main()
+{
+int n,m,l,i=0,j=1;
+printf("Enter the n value:");
+scanf("%d",&n);
+printf("Enter the pair:");
+scanf("%d",&l);
+for(i=0;i<2*n;i++)
+{
+m=2*n-i;
+if(p!=m)
+{
+printf("The pair is%d for days %d is %d\n",l,j++,l);
+}
+}
+getch();
+}
